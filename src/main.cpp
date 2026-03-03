c5a2c842790183d2556178f64e9847b6cd683d32
Mars-by-sundown <nragano@gmail.com>
2026-03-03 16:24:33 -0600

    HelloTriangle starting point

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,48 @@
-#include <vulkan/vulkan.h>
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
+
 #include <iostream>
-#define TINYOBJLOADER_IMPLEMENTATION
-#include <tiny_obj_loader.h>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    // Test Vulkan
-    uint32_t version;
-    vkEnumerateInstanceVersion(&version);
-    std::cout << "Vulkan version: "
-              << VK_VERSION_MAJOR(version) << "."
-              << VK_VERSION_MINOR(version) << "."
-              << VK_VERSION_PATCH(version) << std::endl;
-
-    // Test GLFW
-    if (!glfwInit()) {
-        std::cerr << "GLFW failed to initialize" << std::endl;
-        return -1;
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
     }
-    std::cout << "GLFW version: " << glfwGetVersionString() << std::endl;
-    glfwTerminate();
 
-    // Test GLM
-    glm::vec3 testVec(1.0f, 2.0f, 3.0f);
-    std::cout << "GLM vec3: "
-              << testVec.x << ", "
-              << testVec.y << ", "
-              << testVec.z << std::endl;
+private:
+    void initVulkan() {
 
-    return 0;
+    }
+
+    void mainLoop() {
+
+    }
+
+    void cleanup() {
+
+    }
+};
+
+int main()
+{
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "failed" << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cerr << "success" << std::endl;
+    return EXIT_SUCCESS;
 }
