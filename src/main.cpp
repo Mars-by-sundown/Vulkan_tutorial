d7267a25f6a66383682f7b0b788f8c933e8a7cd7
Mars-by-sundown <nragano@gmail.com>
2026-03-04 13:01:58 -0600

    added findQueueFamilies

--- src/main.cpp
+++ src/main.cpp
@@ -200,6 +200,20 @@ private:
             throw std::runtime_error("failed to find a suitable GPU!");
         }
     }
+
+    uint32_t findQueueFamilies(vk::raii::PhysicalDevice physicalDevice) {
+        // find the index of the first queue family that supports graphics
+        std::vector<vk::QueueFamilyProperties> queueFamilyProperties = physicalDevice.getQueueFamilyProperties();
+
+        // get the first index into queueFamilyProperties which supports graphics
+        auto graphicsQueueFamilyProperty =
+            std::find_if( 
+                queueFamilyProperties.begin(),
+                queueFamilyProperties.end(),
+                []( vk::QueueFamilyProperties const & qfp ) { return qfp.queueFlags & vk::QueueFlagBits::eGraphics; } );
+
+        return static_cast<uint32_t>( std::distance( queueFamilyProperties.begin(), graphicsQueueFamilyProperty ) );
+    }
     
 private:
     GLFWwindow* window;
