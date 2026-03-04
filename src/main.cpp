d579697847a3f4246dcde94744e05c2eea00ccd4
Mars-by-sundown <nragano@gmail.com>
2026-03-04 12:44:22 -0600

    pickPhysicalDevice

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,10 @@ import vulkan_hpp;
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 
+std::vector<const char*> deviceExtensions = {
+    vk::KHRSwapchainExtensionName
+};
+
 const std::vector<char const*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
 };
@@ -24,6 +28,8 @@ constexpr bool enableValidationLayers = false;
 #else
 constexpr bool enableValidationLayers = true;
 #endif
+
+
 
 static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(
     vk::DebugUtilsMessageSeverityFlagBitsEXT       severity,
@@ -49,6 +55,7 @@ private:
     void initVulkan() {
         createInstance();
         setupDebugMessenger();
+        pickPhysicalDevice();
     }
 
     void setupDebugMessenger()
@@ -163,13 +170,43 @@ private:
         return extensions;
     }
 
-
+    void pickPhysicalDevice() {
+        std::vector<vk::raii::PhysicalDevice> devices = instance.enumeratePhysicalDevices();
+        const auto devIter = std::ranges::find_if(devices,
+            [&](auto const & device) {
+                auto queueFamilies = device.getQueueFamilyProperties();
+                bool isSuitable = device.getProperties().apiVersion >= VK_API_VERSION_1_3;
+                const auto qfpIter = std::ranges::find_if(queueFamilies,
+                []( vk::QueueFamilyProperties const & qfp )
+                    {
+                        return (qfp.queueFlags & vk::QueueFlagBits::eGraphics) != static_cast<vk::QueueFlags>(0);
+                    } );
+                isSuitable = isSuitable && ( qfpIter != queueFamilies.end() );
+                auto extensions = device.enumerateDeviceExtensionProperties( );
+                bool found = true;
+                for (auto const & extension : deviceExtensions) {
+                    auto extensionIter = std::ranges::find_if(
+                        extensions,
+                        [extension](auto const & ext) {return strcmp(ext.extensionName, extension) == 0;});
+                    found = found &&  extensionIter != extensions.end();
+                }
+                isSuitable = isSuitable && found;
+                if (isSuitable) {
+                    physicalDevice = device;
+                }
+                return isSuitable;
+        });
+        if (devIter == devices.end()) {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+    }
     
 private:
     GLFWwindow* window;
     vk::raii::Context context;
     vk::raii::Instance instance = nullptr;
     vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+    vk::raii::PhysicalDevice physicalDevice = nullptr;
 };
 
 int main()
