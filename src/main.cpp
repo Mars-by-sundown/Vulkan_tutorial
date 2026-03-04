c8519a9aae244ad9588fa5fff40d1de049a8ac99
Mars-by-sundown <nragano@gmail.com>
2026-03-04 08:33:40 -0600

    Created Vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
+#define VULKAN_HPP_NO_CONSTRUCTORS
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #include <vulkan/vulkan_raii.hpp>
 #else
 import vulkan_hpp;
 #endif
 
-#define GLFW_INCLUDE_VULKAN
+// #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -26,7 +27,7 @@ public:
 
 private:
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -50,8 +51,49 @@ private:
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr,nullptr);
     }
 
+    void createInstance(){
+        constexpr vk::ApplicationInfo appInfo{
+            .pApplicationName   = "Hello Triangle",
+            .applicationVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+            .pEngineName        = "No Engine",
+            .engineVersion      = VK_MAKE_VERSION( 1, 0, 0 ),
+            .apiVersion         = vk::ApiVersion14};
+
+        // Get the required instance extensions from GLFW.
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+        {
+            if (std::ranges::none_of(extensionProperties,
+                                    [glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+                                    { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
+        std::cout << "available extensions:\n";
+        auto extensions = context.enumerateInstanceExtensionProperties();
+        for (const auto& extension : extensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+        
+        vk::InstanceCreateInfo createInfo{};
+        // createInfo.sType = vk::StructureType::eInstanceCreateInfo;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        instance = vk::raii::Instance(context, createInfo);
+    };
+    
 private:
     GLFWwindow* window;
+    vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
 };
 
 int main()
